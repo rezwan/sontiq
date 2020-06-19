6077ee33e51b80d8c1d89b8a06f994d956aa8311
rezwan <rezwan@Nuarca.com>
2020-06-19 13:24:13 +0600

    dropdown

---
+++ Santiq/ClientApp/src/layouts/DefaultLayout.tsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import { Route } from "react-router-dom";
+import NavMenu from './NavMenu';
+interface IDefaultProps {
+    component: any;
+    path?: string;
+    exact?: boolean;
+}
+
+const Default: React.SFC<IDefaultProps> = (props) => {
+    const { component: Component, ...rest } = props;
+    return <Route {...rest} render={matchProps => (
+        <div className="defaultLayout">
+            <NavMenu/>
+            <Component {...matchProps} />
+        </div>
+    )}/>;
+};
+
+export default Default;
